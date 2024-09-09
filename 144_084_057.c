14e221c9f54888af8ad2830e6ca40d143acb6c0e
brrraar <abrarrahmanhakim@gmail.com>
2024-09-09 19:54:20 +0800

    first commit

---
+++ 144_084_057.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int num;
+    scanf("%d", &num);
+        if (num % 2 == 1){
+            printf ("Ganjil");
+        } else{
+            printf("Genap");
+        }
+    return 0;
+}
